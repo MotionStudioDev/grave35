f31661fed69309ca0966057c3f0577b532af2a4a
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2025-09-11 21:41:32 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,24 @@
-const { ActivityType, Events } = require("discord.js")
-module.exports = {
-	name: Events.ClientReady,
-	once: true,
-	execute(client) {
-    let activities = [ `Bakım modu aktif - Grave`, `${client.user.username}` ], i = 0;
-    setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }), 22000);
-}};
+const { ActivityType, Events } = require("discord.js");
 
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    let activities = [
+      `Güçlü sistemler, yeni altyapı, düşük ping`,
+	  `/yardım - v1.0.9 Beta`,
+      `${client.user.username} - Yeniden Sizlerle`,
+    ], i = 0;
+
+    // Durum modunu Rahatsız Etmeyin (dnd) yap
+    client.user.setStatus("dnd");
+
+    // Her 10 saniyede bir aktiviteyi değiştir
+    setInterval(() => {
+      client.user.setActivity({
+        name: activities[i++ % activities.length],
+        type: ActivityType.Playing // "Oynuyor" olarak gözükür
+      });
+    }, 10000);
+  }
+};
